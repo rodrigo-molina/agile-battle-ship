86bc7c438fd48216ad7f66cbcb88ac9c10a20fe6
rodrigo-molina <43792562+rodrigo-molina@users.noreply.github.com>
2019-04-12 00:23:04 +0200

    use implicits in ShipApi: use context bounds instead

--- src/main/scala/agile/battleship/infrastructure/ShipsApi.scala
+++ src/main/scala/agile/battleship/infrastructure/ShipsApi.scala
@@ -2,11 +2,9 @@ package agile.battleship.infrastructure
 
 import agile.battleship.core.ShipsService
 import agile.battleship.core.domain.Ship
-import akka.actor.ActorSystem
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
-import akka.stream.ActorMaterializer
 import spray.json.DefaultJsonProtocol
 
 
@@ -14,7 +12,7 @@ trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val shipFormat = jsonFormat2(Ship)
 }
 
-case class ShipsApi(shipService: ShipsService)(implicit val system: ActorSystem, implicit val materializer: ActorMaterializer) extends JsonSupport {
+case class ShipsApi[ActorSystem, ActorMaterializer](shipService: ShipsService) extends JsonSupport {
   val basePath: String = "ships"
 
   def route: Route = get {
