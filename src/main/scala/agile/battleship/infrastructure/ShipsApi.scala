fe546c4ff201f5a893d33c0bb5e83071d26fb3b2
rodrigo-molina <43792562+rodrigo-molina@users.noreply.github.com>
2019-04-12 00:17:51 +0200

    use implicits in ShipApi

--- src/main/scala/agile/battleship/infrastructure/ShipsApi.scala
+++ src/main/scala/agile/battleship/infrastructure/ShipsApi.scala
@@ -4,29 +4,23 @@ import agile.battleship.core.ShipsService
 import agile.battleship.core.domain.Ship
 import akka.actor.ActorSystem
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.model.Uri.Path
-import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
 import spray.json.DefaultJsonProtocol
 
 
-
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val shipFormat = jsonFormat2(Ship)
 }
 
-case class ShipsApi(shipService: ShipsService) extends JsonSupport {
-  val basePath: Path = Uri.Path("/ships")
-
-  implicit val system = ActorSystem()
-  implicit val materializer = ActorMaterializer()
+case class ShipsApi(shipService: ShipsService)(implicit val system: ActorSystem, implicit val materializer: ActorMaterializer) extends JsonSupport {
+  val basePath: String = "ships"
 
   def route: Route = get {
-      path("ships") {
-        complete(shipService.getAllShips())
-      }
+    path(basePath) {
+      complete(shipService.getAllShips())
+    }
   }
 
 }
